aa73bda0652e48d1875896c7daa2ad184949363c
Megan Lov <mlov2@illinois.edu>
2020-11-16 17:32:55 -0600

    Rename 'particle' files to 'person' and delete code in these files

---
+++ include/core/person.h
@@ -0,0 +1,18 @@
+//
+// Created by Megan Lov on 11/8/20.
+//
+#pragma once
+
+#include "cinder/gl/gl.h"
+
+using glm::vec2;
+using glm::vec3;
+using std::string;
+
+namespace disease {
+
+class Person {
+
+};
+
+}  // namespace disease
---
+++ src/core/person.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Megan Lov on 11/8/20.
+//
+#include "core/person.h"
+
+namespace disease {
+
+}  // namespace disease
