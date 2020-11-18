185049ce5b41c74196966499beab7472eb60e5a1
Megan Lov <mlov2@illinois.edu>
2020-11-17 22:10:21 -0600

    Define the custom constructor for Histogram class

--- src/core/histogram.cpp
+++ src/core/histogram.cpp
@@ -5,4 +5,15 @@
 
 namespace disease {
 
+Histogram::Histogram(const vector<Disease::Person>& particles,
+                     double max_number_of_particles, double container_height,
+                     const vec2& container_top_right_corner) {
+  upper_bound_for_y_ = max_number_of_particles;
+
+  container_height_ = container_height;
+  container_top_right_corner_ = container_top_right_corner;
+  double total_spacing_between_histograms = (kHistogramLimit - 1) * kSpacingBetweenHistograms;
+  histogram_graph_dimension_ = (container_height - total_spacing_between_histograms) / kHistogramLimit;
+}
+
 }  // namespace disease
