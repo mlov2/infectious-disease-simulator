1348612a693ae604d4a3c56089ed1ae544357c72
Megan Lov <mlov2@illinois.edu>
2020-11-17 21:54:40 -0600

    Remove person source and test file from CMakeLists.txt and delete person source, header, and test file

--- src/core/person.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Megan Lov on 11/8/20.
-//
-#include "core/person.h"
-
-namespace disease {
-
-}  // namespace disease
--- tests/test_person.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by Megan Lov on 11/8/20.
-//
-
